f0e44e5ddbf02a9569e615d1dc991794b8db34c1
Gaishi Hirota <mail@gaishi.dev>
2024-06-01 02:00:35 +0900

    chore: add husky and lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1>HogeHoge</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
