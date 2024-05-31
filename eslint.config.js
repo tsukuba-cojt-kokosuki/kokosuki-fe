ba3d60a47c0517f62c307d6befdb3b1ddd1388a5
Gaishi Hirota <mail@gaishi.dev>
2024-06-01 02:47:10 +0900

    chore: configure eslint

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import eslint from "@eslint/js"
+import tseslintParser from "@typescript-eslint/parser"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: {
+        ...globals.browser,
+      },
+      parser: tseslintParser,
+    },
+    plugins: {
+      "react-refresh": reactRefresh,
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      ...reactHooks.configs.recommended.rules,
+    },
+    files: ["**/*.{ts,tsx,js,jsx}"],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+]
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
+/*global module, require*/
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
