f0a67adee07ef8ceac521920a7ccb5edfd0c95ee
Gaishi Hirota <mail@gaishi.dev>
2024-06-19 23:35:55 +0900

    feat: create form

--- src/pages/create/song-list.tsx
+++ src/pages/create/song-list.tsx
@@ -1,15 +1,25 @@
+import { Dispatch, SetStateAction, useState } from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import {
   Table,
   TableBody,
@@ -23,7 +33,7 @@ import { Song } from "./page"
 
 type SongListProps = {
   songs: Song[]
-  setSongs: (songs: Song[]) => void
+  setSongs: Dispatch<SetStateAction<Song[]>>
 }
 
 const SongList = ({ songs, setSongs }: SongListProps) => {
@@ -48,37 +58,111 @@ const SongList = ({ songs, setSongs }: SongListProps) => {
           ))}
         </TableBody>
       </Table>
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="outline">Edit Profile</Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Add </DialogTitle>
-            <DialogDescription>Add a Youtube URL</DialogDescription>
-          </DialogHeader>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label
-                htmlFor="url"
-                className="text-right"
-              >
-                YouTube URL
-              </Label>
-              <Input
-                id="url"
-                defaultValue=""
-                className="col-span-3"
-              />
-            </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit">Add music</Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+      <AddSongDialog setSongs={setSongs} />
     </div>
   )
 }
 
 export { SongList }
+
+const addSongFormSchema = z.object({
+  url: z
+    .string()
+    .min(1, { message: "YouTube の URLを入力してください" })
+    .superRefine((value, context) => {
+      if (!value) {
+        return false
+      }
+
+      const url = new URL(value)
+      const songId = url.searchParams.get("v")
+
+      if (url.hostname !== "www.youtube.com") {
+        context.addIssue({
+          message: "YouTube の URLを入力してください",
+          code: z.ZodIssueCode.custom,
+        })
+      }
+
+      if (!songId) {
+        context.addIssue({
+          message: "URL に Video ID が見つかりません",
+          code: z.ZodIssueCode.custom,
+        })
+      }
+
+      return true
+    }),
+})
+
+type AddSongDialogProps = {
+  setSongs: Dispatch<SetStateAction<Song[]>>
+}
+
+const AddSongDialog = ({ setSongs }: AddSongDialogProps) => {
+  const [open, setOpen] = useState(false)
+
+  const form = useForm<z.infer<typeof addSongFormSchema>>({
+    resolver: zodResolver(addSongFormSchema),
+    defaultValues: {
+      url: "",
+    },
+  })
+
+  const onSubmit = (values: z.infer<typeof addSongFormSchema>) => {
+    const url = new URL(values.url)
+    const songId = url.searchParams.get("v") as string
+
+    setSongs((songs) => {
+      return [
+        ...songs,
+        {
+          songId,
+          url: values.url,
+          startTime: 0,
+          endTime: 60,
+          createDate: new Date(),
+          updateDate: new Date(),
+        },
+      ]
+    })
+
+    setOpen(false)
+  }
+
+  return (
+    <Dialog
+      open={open}
+      onOpenChange={setOpen}
+    >
+      <DialogTrigger asChild>
+        <Button>曲を追加</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>曲を追加</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="url"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>YouTube URL</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter className="mt-3">
+              <Button type="submit">追加</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  )
+}
