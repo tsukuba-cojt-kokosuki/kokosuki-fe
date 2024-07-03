93b68fcc1a6ef675657a9b4ddb88a698c5c67625
Shibaken28 <8128kanzensu@gmail.com>
2024-07-03 15:03:52 +0900

    feat: sliderによって範囲を選択し、その場所が再生されるように

--- src/pages/create/page.tsx
+++ src/pages/create/page.tsx
@@ -1,4 +1,8 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
+import Slider from "@mui/material/Slider"
+import { Play } from "lucide-react"
+import ReactPlayer from "react-player/youtube"
+import YouTubePlayer from "react-player/youtube"
 import { SongList } from "./song-list"
 
 export type Song = {
@@ -26,23 +30,56 @@ const defaultSongs = [
   },
 ]
 
+function valuetext(value: number) {
+  return `${value}°C`
+}
+
+const minDistance = 1
+
 const Create = () => {
   const [songs, setSongs] = useState<Song[]>(defaultSongs)
   const [selectedSong, setSelectedSong] = useState<Song | null>(null)
+  const [value1, setValue1] = useState<[number, number]>([20, 25])
+  const [playing, setPlaying] = useState(true)
+  const player = useRef<YouTubePlayer>(null)
+  const [timeOutId, setTimeOutId] = useState<number>(0)
 
   const handleSongSelect = (song: Song) => {
     setSelectedSong(song)
+    setValue1([song.startTime, song.endTime])
   }
 
-  const getIframeSrc = () => {
-    if (!selectedSong) return ""
-    const { songId, startTime, endTime } = selectedSong
-    return `https://www.youtube.com/embed/${songId}?start=${startTime}&end=${endTime}`
-  }
+  useEffect(() => {
+    if (selectedSong) {
+      setValue1([selectedSong.startTime, selectedSong.endTime])
+    }
+  }, [selectedSong])
 
-  // selectedSong に0番目を代入しておく
-  if (selectedSong === null) {
-    setSelectedSong(songs[0] || null)
+  const handleChange1 = (_event: Event, newValue: number | number[], activeThumb: number) => {
+    if (!Array.isArray(newValue)) {
+      return
+    }
+
+    if (player.current === null) return
+
+    if (activeThumb === 0) {
+      setValue1([Math.min(newValue[0] as number, value1[1] - minDistance), value1[1]])
+      player.current.seekTo(value1[0])
+      setPlaying(true)
+    } else {
+      setValue1([value1[0], Math.max(newValue[1] as number, value1[0] + minDistance)])
+      // 終了時間を設定
+    }
+    clearTimeout(timeOutId)
+    setTimeOutId(
+      setTimeout(
+        () => {
+          setPlaying(false)
+        },
+        (value1[1] - value1[0]) * 1000,
+      ),
+    )
+    console.log((value1[1] - value1[0]) * 1000)
   }
 
   return (
@@ -52,13 +89,20 @@ const Create = () => {
         setSongs={setSongs}
         setSelectedSong={handleSongSelect}
       />
-      <iframe
-        id="player"
-        width="640"
-        height="360"
-        src={getIframeSrc()}
-        frameBorder="0"
-        allowFullScreen
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        ref={player}
+        playing={playing}
+        onPlay={() => setPlaying(true)}
+        onPause={() => setPlaying(false)}
+      />
+      <Slider
+        getAriaLabel={() => "Minimum distance"}
+        value={value1}
+        onChange={handleChange1}
+        valueLabelDisplay="auto"
+        getAriaValueText={valuetext}
+        disableSwap
       />
     </>
   )
