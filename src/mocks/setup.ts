07eeaf5abaece2420fb91d1e2f567b8f32ebcc9a
Gaishi Hirota <mail@gaishi.dev>
2024-07-05 00:04:19 +0900

    chore: bypass unhandled requests

--- src/mocks/setup.ts
+++ src/mocks/setup.ts
@@ -1,10 +1,13 @@
 import { setupWorker } from "msw/browser"
-import { GetUsersMeCrossfades } from "./users/me/crossfades"
 import { GetLatestCrossfades } from "./crossfades/latest/crossfades"
 import { GetPopularCrossfades } from "./crossfades/popular/crossfades"
+import { GetUsersMeCrossfades } from "./users/me/crossfades"
 
 export const setupMsw = async () => {
   if (import.meta.env.DEV && import.meta.env.VITE_MSW_ENABLED === "true") {
-    await setupWorker(...[GetUsersMeCrossfades, GetLatestCrossfades, GetPopularCrossfades]).start()
+    await setupWorker(...[GetUsersMeCrossfades, GetLatestCrossfades, GetPopularCrossfades]).start({
+      // mock にないリクエストは通してあげる
+      onUnhandledRequest: "bypass",
+    })
   }
 }
