dd7bdf5a185c43b9b95119cf482398096bbd7a1c
Gaishi Hirota <mail@gaishi.dev>
2024-07-05 15:55:43 +0900

    reset this commit

--- src/pages/login/pages.tsx
+++ src/pages/login/pages.tsx
@@ -1,3 +1,21 @@
-const Login = () => <h1>login</h1>
+import useSWR from "swr"
+import { Button } from "@/components/ui/button"
+
+const Login = () => {
+  const { data, error, isLoading } = useSWR("/")
+
+  if (isLoading) return <h1>Loading...</h1>
+  if (error) return <h1>Error</h1>
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <p>{JSON.stringify(data)}</p>
+      <Button asChild>
+        <a href={`http://localhost:8787/login?redirect_to=${window.location.origin}`}>Login</a>
+      </Button>
+    </div>
+  )
+}
 
 export default Login
