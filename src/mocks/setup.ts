45358933d097d229ab5a9e32b6cc10b2796f6ff7
Gaishi Hirota <mail@gaishi.dev>
2024-07-10 12:53:27 +0900

    chore: add crossfades/crossfadeId mocks

--- src/mocks/setup.ts
+++ src/mocks/setup.ts
@@ -1,9 +1,10 @@
 import { setupWorker } from "msw/browser"
+import { getCrossfadesCrossfadeId } from "./crossfades/crossfadeId"
 import { GetUsersMeCrossfades } from "./users/me/crossfades"
 
 export const setupMsw = async () => {
   if (import.meta.env.DEV && import.meta.env.VITE_MSW_ENABLED === "true") {
-    await setupWorker(...[GetUsersMeCrossfades]).start({
+    await setupWorker(...[GetUsersMeCrossfades, getCrossfadesCrossfadeId]).start({
       // mock にないリクエストは通してあげる
       onUnhandledRequest: "bypass",
     })
