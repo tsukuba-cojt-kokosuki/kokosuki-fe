1aaf495c3a3d11e9916b6a14be34b0a8f0c2d545
Shibaken28 <8128kanzensu@gmail.com>
2024-07-10 13:36:33 +0900

    feat: 曲の追加をダイアログからではなく直接できるように

--- src/pages/create/page.tsx
+++ src/pages/create/page.tsx
@@ -56,6 +56,16 @@ const defaultSongs = [
   },
 ]
 
+// 保存するときに走らせる関数
+const SaveCrossfade = () => {}
+
+// 完成ボタン
+const Complete = () => {
+  SaveCrossfade()
+  console.log("complete")
+  // 保存後にページ遷移
+}
+
 const Create = () => {
   const [songs, setSongs] = useState<Song[]>(defaultSongs)
   const [selectedSongIndex, setSelectedSongIndex] = useState<number | null>(0)
@@ -84,8 +94,10 @@ const Create = () => {
           />
         </div>
       </div>
-      <Button variant="outline">保存</Button>
-      <Button variant="outline">完成</Button>
+      <div className="flex gap-10 pt-10">
+        <Button onClick={SaveCrossfade}>保存</Button>
+        <Button onClick={Complete}>完成</Button>
+      </div>
     </>
   )
 }
--- src/pages/create/song-list.tsx
+++ src/pages/create/song-list.tsx
@@ -171,43 +171,26 @@ const AddSongDialog = ({ setSongs }: AddSongDialogProps) => {
         },
       ]
     })
-
     setOpen(false)
   }
 
   return (
-    <Dialog
-      open={open}
-      onOpenChange={setOpen}
-    >
-      <DialogTrigger asChild>
-        <Button>曲を追加</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>曲を追加</DialogTitle>
-        </DialogHeader>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-            <FormField
-              control={form.control}
-              name="url"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>YouTube URL</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <DialogFooter className="mt-3">
-              <Button type="submit">追加</Button>
-            </DialogFooter>
-          </form>
-        </Form>
-      </DialogContent>
-    </Dialog>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="url"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>YoutubeのURLで曲を追加</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
   )
 }
