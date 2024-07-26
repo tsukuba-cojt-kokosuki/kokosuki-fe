98ea4846331bfbcbec840072d3e7eb72dbb2cb42
Gaishi Hirota <mail@gaishi.dev>
2024-07-26 19:33:35 +0900

    refactor: define types of environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_MSW_ENABLED?: boolean
+  readonly VITE_BACKEND_URL: string
+  readonly VITE_YOUTUBE_API_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
