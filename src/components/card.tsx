b6fe00a61d7aa9b7b284847c796329c497fe431d
Gaishi Hirota <mail@gaishi.dev>
2024-07-31 04:27:20 +0900

    style: responsive card container

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -20,45 +20,45 @@ const Card = ({ crossfade, showEditButton, showDeleteButton }: CardProps) => {
   const handleLikeClick = () => {
     setLiked(!liked)
   }
+
   return (
-    <>
-      <div className="w-full sm:w-[calc(50%-1rem)] md:w-[calc(33.333%-1rem)] lg:w-[calc(25%-1rem)] xl:w-[calc(20%-1rem)] m-2 shadow-xl hover:bg-stone-600 flex flex-col">
-        <Link
-          to={`/play/${crossfade.id}`}
-          className="flex flex-col flex-grow p-5"
-        >
-          <div className="mx-auto mb-4">
-            <Thumbnail {...crossfade.icon} />
-          </div>
-          <div className="flex flex-col justify-start h-20">
-            <h3 className="text-xl sm:text-lg md:text-xl lg:text-xl line-clamp-3 hover:underline">
-              {crossfade.title}
-            </h3>
-          </div>
-        </Link>
-        <div className="flex px-4 py-4 space-x-2 border-t">
-          <Button
-            variant="ghost"
-            className="p-0 h-fit"
-            onClick={handleLikeClick}
-          >
-            <Heart className={liked ? "fill-current" : ""} />
-          </Button>
-          <ShareCrossfadeDialog crossfade={crossfade} />
-          {showEditButton && <SquarePen />}
-          {showDeleteButton && (
-            <DeleteCrossfadeDialog crossfade={crossfade}>
-              <Button
-                variant="ghost"
-                className="p-0 h-fit"
-              >
-                <Trash2 />
-              </Button>
-            </DeleteCrossfadeDialog>
-          )}
+    <div className="flex flex-col w-full shadow-xl hover:bg-stone-600">
+      <Link
+        to={`/play/${crossfade.id}`}
+        className="flex flex-col flex-grow p-5"
+      >
+        <Thumbnail
+          {...crossfade.icon}
+          className="flex-grow-0 flex-shrink-0 w-24 h-24 mx-auto mb-2 aspect-square lg:h-40 lg:w-40"
+        />
+        <div className="flex-grow">
+          <h3 className="text-xl sm:text-lg md:text-xl lg:text-xl line-clamp-3 hover:underline">
+            {crossfade.title}
+          </h3>
         </div>
+      </Link>
+      <div className="flex px-4 py-4 space-x-2 border-t">
+        <Button
+          variant="ghost"
+          className="p-0 h-fit"
+          onClick={handleLikeClick}
+        >
+          <Heart className={liked ? "fill-current" : ""} />
+        </Button>
+        <ShareCrossfadeDialog crossfade={crossfade} />
+        {showEditButton && <SquarePen />}
+        {showDeleteButton && (
+          <DeleteCrossfadeDialog crossfade={crossfade}>
+            <Button
+              variant="ghost"
+              className="p-0 h-fit"
+            >
+              <Trash2 />
+            </Button>
+          </DeleteCrossfadeDialog>
+        )}
       </div>
-    </>
+    </div>
   )
 }
 
